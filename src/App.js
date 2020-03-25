67f9f730574fc60a3422137f85a381aea4ea3bd5
kushal-kamra <kushalkamra@gmail.com>
2020-03-26 01:16:39 +0530

    Login Screen State and Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import Login from './components/Login';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      login: 0
+    }
+  }
+  
+  render() {
+    if (this.state.login === 0) {
+      return (
+        <div>
+          <Login></Login>
+        </div>
+      );      
+    } else {
+      return (
+        <div>
+          <h1>Without Login</h1>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
