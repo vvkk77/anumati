4d77564b0b48b09ca2751094fe9be2dd9b223043
Vishwajeet Kushmariya <k.vish.7777@gmail.com>
2020-03-26 22:46:28 +0530

    Updates

---
+++ precache-manifest.0fd0333c8a64c99332ef3f24cb111f31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ef4ac1acf8b1f841da51894c67d7d1",
+    "url": "/anumati_front/index.html"
+  },
+  {
+    "revision": "62d636f1146df2d9caac",
+    "url": "/anumati_front/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9a28f1a143f1e99b31ec",
+    "url": "/anumati_front/static/css/main.1c198f6b.chunk.css"
+  },
+  {
+    "revision": "62d636f1146df2d9caac",
+    "url": "/anumati_front/static/js/2.5d15534d.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/anumati_front/static/js/2.5d15534d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a28f1a143f1e99b31ec",
+    "url": "/anumati_front/static/js/main.13f88dd9.chunk.js"
+  },
+  {
+    "revision": "733d2215653f34e85a1e",
+    "url": "/anumati_front/static/js/runtime-main.cd20073d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ecc019a125e8cfb9d663426eb2d8a0bc.js"
+  "/anumati_front/precache-manifest.0fd0333c8a64c99332ef3f24cb111f31.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anumati_front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
