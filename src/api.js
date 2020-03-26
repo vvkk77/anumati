351a3e6155ae68e18cae07f265f63ee0dc61d480
Nitish Alodia <nitish@vahan.co>
2020-03-27 00:20:01 +0530

    cors header added

--- src/api.js
+++ src/api.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://13.233.33.10:8091';
-
+const headers = {
+    "Content-Type": "application/json"
+};
 axios.defaults.baseURL = BASE_URL;
 
 export default {
     signIn(email, password) {
-        return axios.post('/signin', { email, password });
+        return axios.post('/signin', { email, password }, {headers: headers});
     },
 
     register(name, email, password) {
-        return axios.post('/createAccount', { name, email, password, key: 'test' });
+        return axios.post('/createAccount', { name, email, password, key: 'test' }, {headers: headers});
     },
 
     createOrder(type, file,  authToken){
